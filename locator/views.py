a8cd2bb282efaf7b0ea9fdc1799ca98cd1bc51d5
duncan <duncanndiithi@yahoo.com>
2021-07-28 14:34:55 +0300

    endpoint service module

--- locator/views.py
+++ locator/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from mfs_locator.serializers import UserSerializer, GroupSerializer,LocatorSerializer
 
-# Create your views here.
+from locator.models import Locator
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class LocatorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows locator to be viewed or edited.
+    """
+    queryset = Locator.objects.all().order_by('-created')
+    serializer_class = LocatorSerializer
+    permission_classes = [permissions.IsAuthenticated]
