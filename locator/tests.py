ec1df88b4abad9f3245cf298e7a59e47a5a891f2
duncan <duncanndiithi@yahoo.com>
2021-07-28 20:10:38 +0300

    add test cases

--- locator/tests.py
+++ locator/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+import json
+import logging
+from django.urls import reverse
+from locator.models import Locator
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
+# from requests.auth import HTTPBasicAuth
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+from rest_framework.test import APIClient
+
+
+class PostPointsCreateAPIView(APITestCase):
+    def setUp(self) -> None:
+        self.url = 'http://localhost:8000/locator/'
+        self.content_type = "application/json"
+        self.superuser = User.objects.create_superuser(
+            'mfsuser', 'mfsuser@gmail.com', 'I@mtowers26')
+        self.client.login(username='mfsuser', password='I@mtowers26')
+
+    def test_record_created(self):
+        self.assertEquals(
+            Locator.objects.count(),
+            0
+        )
+        data = {
+            'points': '(2,3),(1,1),(5,4),(2,3),(1,1),(5,4)'
+        }
+        response = self.client.post(
+            self.url, data=data, CONTENT_TYPE=self.content_type)
+        # data = json.loads(response.content)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_post(self):
+
+        data = {
+            'points': '(2,3),(1,1),(5,4),(2,3),(1,1),(5,4)'
+        }
+        response = self.client.post(
+            self.url, data=data, CONTENT_TYPE=self.content_type)
+
+        self.assertEquals(
+            Locator.objects.count(),
+            1
+        )
+
+    def test_accurate_adjuscent_point(self):
+
+        data = {
+            'points': '(2,3),(1,1),(5,4),(2,3),(1,1),(5,4)'
+        }
+        response = self.client.post(
+            self.url, data=data, CONTENT_TYPE=self.content_type)
+
+        locator = Locator.objects.first()
+        self.assertEquals(
+            locator.adjuscent,
+            "[2 3],[1 1]"
+        )
--- mfs_locator/settings.py
+++ mfs_locator/settings.py
@@ -134,7 +134,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'file': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': 'debug.log',
         },
