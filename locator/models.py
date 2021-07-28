2be329f2febf1eb10c8c636d37c77c2df62fe2f1
duncan <duncanndiithi@yahoo.com>
2021-07-28 14:16:09 +0300

    remove un needed field

--- locator/models.py
+++ locator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Locator(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    points = models.TextField()
+    adjuscent = models.TextField()
+
+    class Meta:
+        ordering = ['created']
